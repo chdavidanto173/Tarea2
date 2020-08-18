3668eeec80404ca6e090549a9906639415d3c7ff
chdavidanto173 <davidchacon93@hotmail.com>
2020-08-17 21:45:10 -0600

    First commit
    First commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/chdavidanto173/Tarea1
+
+require (
+	github.com/gofrs/uuid v3.3.0+incompatible
+	github.com/golang/protobuf v1.4.2
+	google.golang.org/grpc v1.27.0
+	google.golang.org/protobuf v1.25.0
+)
+
+go 1.12
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+
+	pb "github.com/chdavidanto173/Tarea1"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	lis, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterBookInfoServer(s, &server{})
+
+	log.Printf("Starting gRPC listener on port " + port)
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
